f3fc5662cfd3dcf6f5c55403b60032f6f7780d0a
Do-Tien-Manh <manhdt260599@gmail.com>
2020-11-18 22:36:13 +0700

    update

--- scripts/CalGPA.js
+++ scripts/CalGPA.js
@@ -1,47 +1,30 @@
 //properties not include GPA
 const NOT_IS_CREDIT = ["0",""];
-const NOT_SUBJECT_START_CODE = ["VOV"];
+const NOT_SUBJECT_START_CODE = ["VOV124","VOV134","VOV114","LAB101","LAB101","LAB211","LAB221","LAB231"];
 //properties include GPA
 const STATUS = "Passed";
 
+//selector to tr tag in table
+const SELECTOR_TO_TR = "#ctl00_mainContent_divGrade > :first-child tbody tr";
 
-/**
- * 
- * @param {*} elements
- */
-// function isUndefined(...elements){
-//     for (const elem of elements) {
-//         if(elem === undefined){
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+const PLACE_INJECT_ELEMENT_GPA = "ctl00_mainContent_lblRollNumber";
+
+
 
 /**
  * check condition
  */
 function checkCondition(element){
-    // let x = element.childNodes[8].hasChildNodes();
-    // //check undefined
-    // if(isUndefined([element.childNodes[3],element.childNodes[7],element.childNodes[9].childNodes[0],element.childNodes[8].childNodes[0]])){
-    //     return false;
-    // }
 
-    // let ele = element.childNodes[3];
-    // let subjectCode = element.childNodes[3].innerText.trim();
-    // let credit = element.childNodes[7].innerText.trim();
-    // let status = element.childNodes[9].childNodes[0].innerText.trim();
-    // let point = element.childNodes[8].childNodes[0].innerText.trim();
-
-    if(element.childNodes[3].innerText.trim().startsWith(NOT_SUBJECT_START_CODE[0])){
+    //check subject
+    if(NOT_SUBJECT_START_CODE.includes(element.childNodes[3].innerText.trim())){
         return false;
-    }else if(element.childNodes[9].childNodes[0].innerText.trim() != STATUS){
+    }else if(element.childNodes[9].childNodes[0].innerText.trim() != STATUS){ //check status
         return false;
-    }else if(element.childNodes[8].childNodes[0].innerText.trim() == ""){
+    }else if(element.childNodes[8].childNodes[0].innerText.trim() == ""){ //check point
         return false;
     }
-    else if(NOT_IS_CREDIT.includes(element.childNodes[7].innerText.trim())){
+    else if(NOT_IS_CREDIT.includes(element.childNodes[7].innerText.trim())){ // check credit 0
         return false;
     }
     else{
@@ -57,7 +40,7 @@ function calGPA(){
     let totalPoint = 0;
     let totalRecord = 0;
 
-    let allTrElement = document.querySelectorAll("#ctl00_mainContent_divGrade > :first-child tbody tr");
+    let allTrElement = document.querySelectorAll(SELECTOR_TO_TR);
 
     for(let i = 0 ;i < allTrElement.length; i++){
         if(checkCondition(allTrElement[i])){
@@ -72,12 +55,30 @@ function calGPA(){
 }
 
 /**
+ * 
+ * @param {*} text 
+ */
+function calClassification(gpa){
+    let classification = "";
+    if(gpa >= 9){
+        classification = "Xuat Xac";
+    }else if(gpa >= 8 ){
+        classification = "Gioi";
+    }else if(gpa >= 7){
+        classification = "Kha";
+    }else if(gpa >= 5){
+        classification = "Trung Binh";
+    }
+    return classification;
+}
+
+/**
  * create element
  */
-function createHTML(){
+function createHTML(text){
     let element = document.createElement("span");
     element.className = "label label-default";
-    element.innerText = "GPA: "+ calGPA();
+    element.innerText = text;
     return element;
 }
 
@@ -85,10 +86,15 @@ function createHTML(){
  * inject html to page
  */
 function injectHtml(){
-    let elem = document.getElementById("ctl00_mainContent_lblRollNumber");
-    let injectElement= createHTML();
+    let elem = document.getElementById(PLACE_INJECT_ELEMENT_GPA);
+    let gpa = calGPA();
+    let injectElement_GPA= createHTML("GPA: " + gpa);
+    let injectElement_XL = createHTML("XL: " + calClassification(gpa));
     elem.innerHTML = elem.innerHTML + " - ";
-    elem.append(injectElement);
+    elem.append(injectElement_GPA);
+    elem.innerHTML = elem.innerHTML + " - ";
+    elem.append(injectElement_XL);
+    
 }
 
 //call function
